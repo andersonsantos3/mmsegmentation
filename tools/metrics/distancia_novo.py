edba876571df5a9ecb9e1cc1fc5d6d93c26dca1a
anderson <anderson.asantos3@gmail.com>
2023-08-27 11:47:10 -0400

    feat: abre arquivo json e retorna os dados

--- tools/metrics/distancia_novo.py
+++ tools/metrics/distancia_novo.py
@@ -1,8 +1,17 @@
+from json import load
 from os import environ
+from typing import Union
+
+
+def carregar_json(caminho_arquivo: str) -> Union[dict, list]:
+    with open(caminho_arquivo, 'r', encoding='utf_8') as arquivo:
+        dados = load(arquivo)
+    return dados
 
 
 def main():
-    pass
+    anotacoes_imagens = carregar_json(environ.get('CAMINHO_ANOTACOES_IMAGENS'))
+    predicoes_deteccoes = carregar_json(environ.get('CAMINHO_PREDICOES_DETECCOES'))
 
 
 if __name__ == '__main__':
