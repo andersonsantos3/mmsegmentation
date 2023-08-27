9c8db6f2cdf6321f21661b32021bfbb0455b0e73
anderson <anderson.asantos3@gmail.com>
2023-08-27 15:06:17 -0400

    feat: filtra predicoes por score

--- tools/metrics/distancia_novo.py
+++ tools/metrics/distancia_novo.py
@@ -10,6 +10,11 @@ def carregar_json(caminho_arquivo: str) -> Union[dict, list]:
     return dados
 
 
+def filtrar_predicoes_por_score(predicoes: list, score: float) -> list:
+    predicoes = [predicao for predicao in predicoes if predicao['score'] >= score]
+    return predicoes
+
+
 def obter_anotacoes_por_imagem_id(anotacoes: dict) -> dict:
     annotations = defaultdict(list)
     for annotation in anotacoes['annotations']:
@@ -27,10 +32,13 @@ def obter_predicoes_por_imagem_id(predicoes: dict) -> dict:
 def main():
     anotacoes_imagens = carregar_json(environ.get('CAMINHO_ANOTACOES_IMAGENS'))
     predicoes_deteccoes = carregar_json(environ.get('CAMINHO_PREDICOES_DETECCOES'))
+    score_minimo = float(environ.get('SCORE_MINIMO'))
+    predicoes_deteccoes = filtrar_predicoes_por_score(predicoes_deteccoes, score_minimo)
 
     categorias = anotacoes_imagens['categories']
     anotacoes_por_imagem_id = obter_anotacoes_por_imagem_id(anotacoes_imagens)
     predicoes_por_imagem_id = obter_predicoes_por_imagem_id(predicoes_deteccoes)
+    print()
 
 
 if __name__ == '__main__':
