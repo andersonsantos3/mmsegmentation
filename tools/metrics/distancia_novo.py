6e91ac29f808d922ef423022078e447d5ffeb70b
anderson <anderson.asantos3@gmail.com>
2023-09-02 11:21:37 -0400

    feat: lê arquivo json

--- tools/metrics/distancia_novo.py
+++ tools/metrics/distancia_novo.py
@@ -1,3 +1,13 @@
+from json import load
+from typing import Union
+
+
+def carregar_json(caminho_arquivo: str) -> Union[dict, list]:
+    with open(caminho_arquivo, 'r', encoding='utf_8') as arquivo:
+        dados = load(arquivo)
+    return dados
+
+
 def main():
     pass
 
