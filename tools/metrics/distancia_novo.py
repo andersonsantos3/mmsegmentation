d9f43292b927547075c40a100669e85801786797
anderson <anderson.asantos3@gmail.com>
2023-09-04 09:47:57 -0400

    feat: remove predições com score abaixo de um limiar

--- tools/metrics/distancia_novo.py
+++ tools/metrics/distancia_novo.py
@@ -1,4 +1,5 @@
 from json import load
+from os import environ
 from typing import Union
 
 
@@ -8,8 +9,19 @@ def carregar_json(caminho_arquivo: str) -> Union[dict, list]:
     return dados
 
 
+def remover_predicoes_com_score_baixo(predicoes: dict):
+    score_minimo = float(environ.get('SCORE_MINIMO'))
+    for key, predicoes_ in predicoes.items():
+        for i, boxes in enumerate(predicoes_):
+            predicoes_[i] = [box for box in boxes if box[-1] >= score_minimo]
+        predicoes[key] = predicoes_
+
+
 def main():
-    pass
+    anotacoes_subimagens = carregar_json(environ.get('ARQUIVO_ANOTACOES_SUBIMAGENS'))
+    deteccoes_subimagens = carregar_json(environ.get('ARQUIVO_DETECCOES_SUBIMAGENS'))
+
+    remover_predicoes_com_score_baixo(deteccoes_subimagens)
 
 
 if __name__ == '__main__':
